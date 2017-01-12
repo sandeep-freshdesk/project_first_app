5c34bbb49881c511ec36483026b811050ee3b00b
sandeep-freshdesk <sandeep@freshdesk.com>
2017-01-12 17:54:47 +0530

    first time adding first_app files into git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f05e50c44659711954f036fdba27934364a15fff92c15baf4d3fc15f8481868b0a3477329ef847df9c10b1225c205863a9e866a40bebe89ac11409bf90e840dc'
